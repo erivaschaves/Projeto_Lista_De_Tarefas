33b7742ccb7c2a3c47565fa75a5a69b5d5a61aa0
ErivaldoChaves <erivas_12@hotmail.com>
2024-08-01 10:02:30 -0300

    inserindo funções

--- Js/script.js
+++ Js/script.js
@@ -4,8 +4,34 @@
 
 //Variáveis ​​definidas com const tem escopo de bloco.
 
+//Função seta =>
+
 const cloneDeModeloDeTarefa = () =>{
-    const modeloDeTarefa = window.document.querySelector ('.moledo .listaDeItens')
-    const cloneDeModeloDeTarefa = modelo.cloneNode(true)
+    //criando modelo de tarefa.
+    const modeloDeTarefa = window.document.querySelector ('.modelos .listaDeItens')
+    //clonando modelo de tarefa com função clone node.
+    const cloneDeModeloDeTarefa = modeloDeTarefa.cloneNode(true)
+    //retornando modelo de tarefa.
     return cloneDeModeloDeTarefa
+}
+//inserindo conteudo
+const inserirConteudo = (tarefa, valor)=>{
+    const textoDaTarefa = tarefa.querySelector('.textoDaLista')
+    textoDaTarefa.textContent = valor
+
+}
+//exibir tarefa
+const colocarTarefaNatela = () => {
+    const input = window.document.querySelector('todoInputDeBusca')
+    //removendo espaços nas extremidades com a função trim()
+    const valor = input.value.trim()
+    //caso o valor não seja vazio
+    if (valor!== ''){
+        const tarefa = cloneDeModeloDeTarefa()
+        inserirConteudo(tarefa, valor)
+        window.document.querySelector('todaLista').appendChild(tarefa)
+        input.value = '';
+        adicionarTarefaFeita()
+    }
+
 }
